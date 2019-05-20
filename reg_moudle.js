3f138375c3ef141485e787dae017a32eed76ae2f
נופר טורטלטאוב <nofartu@auth.ad.bgu.ac.il>
2019-05-20 16:36:26 +0300

    first try

---
+++ reg_moudle.js
@@ -0,0 +1,12 @@
+async function Register(username, firstname,lastname, city, country,email,interests,question,answer,password) {
+	try {
+	const user = await getUser(1);
+	const interests = await getInterests(user.userId);
+	const location = await getLocation(interests[0]);
+	console.log(location);
+	} catch (error) {
+		console.log(error.message);
+	}
+}
+
+displayLocation();
