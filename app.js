ca76c8f800a3253e6b307060bd07b6c9af881f35
מאיה מרקוביץ <mayamark@auth.ad.bgu.ac.il>
2019-05-21 16:16:19 +0300

    maya changes - select

--- app.js
+++ app.js
@@ -8,24 +8,72 @@ app.listen(port, function () {
 });
 
 
-app.get('/reg', function(req, res){
+app.get('/reg', function (req, res) {
     DButilsAzure.execQuery("INSERT INTO PoiUsers (username,poiId) VALUES ('abc',1)")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 })
 
-app.get('/select', function(req, res){
+app.get('/select', function (req, res) {
     DButilsAzure.execQuery("SELECT * FROM tableName")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
+})
+
+app.post('/restore', function (req, res) {
+    var notExist = false;
+    DButilsAzure.execQuery("SELECT username FROM Users where username  = '" + req.body.username + "' and question= '" + req.body.quastion + "' and answer= '" + req.body.answer + "'")
+        .then(function (result) {
+            if (result != null) {
+                DButilsAzure.execQuery("SELECT password FROM Passwd where username  = '" + req.body.username + "'")
+                    .then(function (result) {
+                        if (result != null) {
+                            console.log(result.body.password)
+                            res.send(result.body.password)
+                        }
+                        else {
+                            console.log(notExist)
+                            res.send(notExist)
+                        }
+                    })
+                    .catch(function (err) {
+                        console.log(err)
+                        res.send(err)
+                    })
+            }
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+app.post('/retriveConfirmationQuestion', function (req, res) {
+    var notExist = false;
+    DButilsAzure.execQuery("SELECT question FROM Users where username  = '" + req.body.username + "'")
+        .then(function (result) {
+            if (result != null) {
+                console.log(result.body.question)
+                res.send(result.body.question)
+            }
+            else {
+                console.log(notExist)
+                res.send(notExist)
+            }
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 })
