0c9f4ffc0da5ff9c8aeaaea1781a8f4d0e905332
מאיה מרקוביץ <mayamark@auth.ad.bgu.ac.il>
2019-05-21 17:11:44 +0300

    checking post function

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var DButilsAzure = require('./DButils');
+var parse=require("body-parser");
 
 var port = 3000;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
+app.use(parse.urlencoded({extended:true}));
 
 
 app.get('/Register', function(req, res){
@@ -43,14 +45,17 @@ app.get('/select', function (req, res) {
 
 app.post('/restore', function (req, res) {
     var notExist = false;
-    DButilsAzure.execQuery("SELECT username FROM Users where username  = '" + req.body.username + "' and question= '" + req.body.quastion + "' and answer= '" + req.body.answer + "'")
+    var username=req.body.username;
+    var quastion=req.body.question;
+    var answer=req.body.answer;
+    DButilsAzure.execQuery("SELECT username FROM Users where username  = '" + username + "' and question= '" + quastion + "' and answer= '" + answer + "'")
         .then(function (result) {
             if (result != null) {
-                DButilsAzure.execQuery("SELECT password FROM Passwd where username  = '" + req.body.username + "'")
+                DButilsAzure.execQuery("SELECT password FROM Passwd where username  = '" + username + "'")
                     .then(function (result) {
                         if (result != null) {
-                            console.log(result.body.password)
-                            res.send(result.body.password)
+                            console.log(result.query.password)
+                            res.send(result.query.password)
                         }
                         else {
                             console.log(notExist)
