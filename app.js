ea4442a6de53dfc8c461fa1576186d80bbbc5fa8
מאיה מרקוביץ <mayamark@auth.ad.bgu.ac.il>
2019-05-22 23:38:27 +0300

    update delete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var DButilsAzure = require('./DButils');
 var parse = require("body-parser");
-var jwt =require("jsonwebtoken");
+var jwt = require("jsonwebtoken");
 
 var port = 3000;
 app.listen(port, function () {
@@ -10,40 +10,40 @@ app.listen(port, function () {
 });
 app.use(parse.json())
 app.use(parse.urlencoded({ extended: true }));
-secret="thisIsHell"
+secret = "thisIsHell"
 
-app.use("/private", (req, res,next) =>{
-	const token = req.header("x-auth-token");
-	// no token
-	if (!token){
+app.use("/private", (req, res, next) => {
+    const token = req.header("x-auth-token");
+    // no token
+    if (!token) {
         res.status(401).send("Access denied. No token provided.");
-    } 
-	// verify token
-	try {
-		const decoded = jwt.verify(token, secret);
+    }
+    // verify token
+    try {
+        const decoded = jwt.verify(token, secret);
         req.decoded = decoded;
-        var username=req.decoded.name;
-        req.username=username;
+        var username = req.decoded.name;
+        req.username = username;
         DButilsAzure.execQuery("SELECT username FROM Users where username  = '" + username + "'")
-        .then(function (result) {
-            if (result.length > 0) {
-                console.log(true)
-                // res.send(result)
-                next(); //move on to the actual function
-            }
-            else {
-                console.log(notExist)
-                // res.send(notExist)
-            }
-        })
-        .catch(function (err) {
-            console.log(err)
-            res.send(err)
-        })
-		
-	} catch (exception) {
-		res.status(400).send("Invalid token.");
-	}
+            .then(function (result) {
+                if (result.length > 0) {
+                    console.log(true)
+                    // res.send(result)
+                    next(); //move on to the actual function
+                }
+                else {
+                    console.log(notExist)
+                    // res.send(notExist)
+                }
+            })
+            .catch(function (err) {
+                console.log(err)
+                res.send(err)
+            })
+
+    } catch (exception) {
+        res.status(400).send("Invalid token.");
+    }
 });
 
 app.post('/Register', function (req, res) {
@@ -58,31 +58,31 @@ app.post('/Register', function (req, res) {
     var question2 = req.body.question2;
     var answer2 = req.body.answer2;
     var password = req.body.password;
-    var interestString=req.body.interests;
+    var interestString = req.body.interests;
     DButilsAzure.execQuery("INSERT INTO Users (username,first_name,last_name,city,country,email,question1,answer1,question2,answer2) VALUES ('" + username + "','" + fname + "','" + lname + "','" + city + "','" + country + "','" + email + "','" + question1 + "','" + answer1 + "','" + question2 + "','" + answer2 + "')")
-    .then(function (result) {
-        DButilsAzure.execQuery("INSERT INTO Passwd (username,passwd) VALUES ('" + username + "','" + password + "')")
         .then(function (result) {
-            var arr=interestString.split(',');
-            var i;
-            for(i=0;i<arr.length;i++){
-                DButilsAzure.execQuery("INSERT INTO Interests (username,interest) VALUES ('" + username + "','" + arr[i] + "')") 
+            DButilsAzure.execQuery("INSERT INTO Passwd (username,passwd) VALUES ('" + username + "','" + password + "')")
                 .then(function (result) {
-                    res.send(true)
+                    var arr = interestString.split(',');
+                    var i;
+                    for (i = 0; i < arr.length; i++) {
+                        DButilsAzure.execQuery("INSERT INTO Interests (username,interest) VALUES ('" + username + "','" + arr[i] + "')")
+                            .then(function (result) {
+                                res.send(true)
+                            })
+                            .catch(function (err) {
+                                console.log(err)
+                            })
+                    }
                 })
                 .catch(function (err) {
-                    console.log(err)            
+                    console.log(err)
                 })
-            }
         })
         .catch(function (err) {
-            console.log(err)        
+            console.log(err)
+            res.send(false)
         })
-    })
-    .catch(function (err) {
-        console.log(err)
-        res.send(false)
-    })     
 })
 
 
@@ -93,8 +93,8 @@ app.post('/addPoi', function (req, res) { //add poi to poi table
     var category = req.body.category;
     var descr = req.body.descr;
     //ar views = parseInt(req.body.views);
-    var pic=req.body.picture;
-    DButilsAzure.execQuery("INSERT INTO Poi (poiname,rnk,city,category,descr,viw,numRank,picture) VALUES ('" + poiname + "','" + null + "','" + city + "','" + category + "','" + descr + "','" + 0 + "','"+ 0 +"','"+pic+"')")
+    var pic = req.body.picture;
+    DButilsAzure.execQuery("INSERT INTO Poi (poiname,rnk,city,category,descr,viw,numRank,picture) VALUES ('" + poiname + "','" + null + "','" + city + "','" + category + "','" + descr + "','" + 0 + "','" + 0 + "','" + pic + "')")
         .then(function (result) {
             res.send(true)
         })
@@ -132,46 +132,43 @@ app.post('/saveReviewPoi', function (req, res) {
         })
 })
 
-app.post('/saveRankPoi', function(req, res){
-    var poiId=parseInt(req.body.poiId);
-    var rank=parseInt(req.body.rank);
-    DButilsAzure.execQuery("SELECT rnk,numRank FROM Poi WHERE poiId='"+poiId+"'")
-    .then(function(result){         
-        var calcRank=(result[0].rnk*result[0].numRank+rank)/(result[0].numRank+1);
-        var addedRank=result[0].numRank+1;
-        DButilsAzure.execQuery("UPDATE Poi SET rnk = '"+calcRank+"',numRank = '"+addedRank+"' WHERE poiId='"+poiId+"';")
-        .then(function(result1){
-            res.send(true)
+app.post('/saveRankPoi', function (req, res) {
+    var poiId = parseInt(req.body.poiId);
+    var rank = parseInt(req.body.rank);
+    DButilsAzure.execQuery("SELECT rnk,numRank FROM Poi WHERE poiId='" + poiId + "'")
+        .then(function (result) {
+            var calcRank = (result[0].rnk * result[0].numRank + rank) / (result[0].numRank + 1);
+            var addedRank = result[0].numRank + 1;
+            DButilsAzure.execQuery("UPDATE Poi SET rnk = '" + calcRank + "',numRank = '" + addedRank + "' WHERE poiId='" + poiId + "';")
+                .then(function (result1) {
+                    res.send(true)
+                })
+                .catch(function (err) {
+                    console.log(err)
+                    res.send(false)
+                })
+            //res.send(true)
         })
-        .catch(function(err){
+        .catch(function (err) {
             console.log(err)
             res.send(false)
         })
-        //res.send(true)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(false)
-    })
 })
 
-app.delete('/private/deleteUserPoi', function(req, res){ //update delete!!
-    var username=req.username;
-    var poiId=parseInt(req.body.id);
-    DButilsAzure.execQuery("DELETE FROM userPoi WHERE username='"+username+"' AND poiId='"+poiId+"' ")
-    .then(function(result){
-        res.send(true)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
-})
-
-app.get('/select', function (req, res) {
-    DButilsAzure.execQuery("SELECT * FROM tableName")
+app.delete('/private/deleteUserPoi', function (req, res) {
+    var username = req.username;
+    var poiId = parseInt(req.body.poiId);
+    DButilsAzure.execQuery("DELETE FROM userPoi OUTPUT deleted.* WHERE username='" + username + "' AND poiId='" + poiId + "' ")
         .then(function (result) {
-            res.send(result)
+            if (result.length > 0) {
+                console.log(result)
+                res.send(true)
+            }
+            else {
+                console.log(false)
+                res.send(false)
+            }
+
         })
         .catch(function (err) {
             console.log(err)
@@ -235,7 +232,7 @@ app.get('/retriveConfirmationQuestion', function (req, res) {
         })
 })
 
-app.get('/getRandomPoi', function (req, res) { 
+app.get('/getRandomPoi', function (req, res) {
     var notExist = false;
     var minRank = req.body.rank;
     var quer = "SELECT Poi.poiId, poiname, rnk, city, category, descr, viw, picture FROM Poi";
@@ -438,17 +435,17 @@ app.get('/private/getPOIbyInterests', function (req, res) {
                     for (var i = 1; i < len; i++) {
                         var second = result[i];
                         if (second.category != first.category) {
-                            console.log(first,second)
-                            var ret=[first,second];
+                            console.log(first, second)
+                            var ret = [first, second];
                             res.send(ret)
                             break;
                         }
                     }
-                }else{
+                } else {
                     console.log(first)
                     res.send(first)
                 }
-               
+
             }
             else {
                 console.log(notExist)
